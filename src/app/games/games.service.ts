fde717cb37e22ba0cd019ed0dc02628b04b0c4fc
Darío Villena <iloveyoudoctorzaius@gmail.com>
2018-09-16 12:19:24 +0200

    Using HttpParams in getAllTodayGames

--- src/app/games/games.service.ts
+++ src/app/games/games.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Game } from './game.interface';
 
-const API_IGDB = 'https://api-endpoint.igdb.com/games';
+const API_IGDB = 'https://api-endpoint.igdb.com/games/';
 
 // https://api-endpoint.igdb.com/games/?fields=name,first_release_date&filter[release_dates.date][eq]=2013-09-17
 @Injectable({
@@ -11,7 +11,7 @@ const API_IGDB = 'https://api-endpoint.igdb.com/games';
 })
 export class GamesService {
 
-  allDates: String;
+  allDates: string;
 
   constructor(
     private http: HttpClient
@@ -21,13 +21,20 @@ export class GamesService {
     const month = `${today.getMonth() + 1}`;
     const day = `${today.getDate()}`;
     this.allDates = '';
-    for (let i = 1970; i < year; i++) {
+    for (let i = 1951; i < year; i++) {
       this.allDates += `${i}-${month.padStart(2, '0')}-${day.padStart(2, '0')},`;
     }
     this.allDates = this.allDates.substring(0, this.allDates.length - 1);
   }
 
   getAllTodayGames(): Observable<Game[]> {
-    return this.http.get<Game[]>(`${API_IGDB}/?fields=name,rating,popularity,cover&filter[first_release_date][any]=${this.allDates}&limit=50`);
+    let params = new HttpParams();
+    params = params.append('fields', 'name,rating,popularity,cover');
+    params = params.append('filter[first_release_date][any]', this.allDates);
+    params = params.append('limit', '50');
+    params = params.append('order', 'first_release_date:asc');
+    params = params.append('order', 'popularity:desc');
+
+    return this.http.get<Game[]>(API_IGDB, {params: params});
   }
 }
